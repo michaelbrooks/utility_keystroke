052d42fbbf62883bae8150e720ac57b263e5ae78
Michael Brooks <mjbrooks@uw.edu>
2013-06-04 03:43:44 +0000

    analysis scripts

--- web2py/applications/utility/controllers/keystroke_analysis.py
+++ web2py/applications/utility/controllers/keystroke_analysis.py
@@ -1,97 +1,47 @@
 response.generic_patterns = ['html', 'csv']
 
 import random, hashlib, uuid, datetime, time, math
-import gluon.contrib.simplejson
-import re, csv
+import cStringIO
 
-def ks_entry_surveys():
+def index():
+    # print out a list of available data files
+    links = []
     
-    entries = db().select(
-        db.ks_entry_surveys.ALL, 
-        db.conditions.json,
-        join=db.conditions.on(db.conditions.id == db.ks_entry_surveys.condition)
-    )
-    return dict(data=as_obj(entries))
-    # return dict(data=entries.as_list(), export_to_csv_file=export_to_csv_file)
-
-def ks_post_surveys():
-    entries = db().select(db.ks_entry_surveys.ALL);
-    return dict(data=entries)
-
-def as_obj(rows, mode='object', default=None):
-    """
-    serializes the table to a list of objects
-    """
-
-    items = [record.as_dict() for record in rows]
-
-    return items
+    linkTemps = [
+        'keystroke_analysis/entry_surveys/%d',
+        'keystroke_analysis/post_surveys/%d'
+    ]
     
-# def export_to_csv_file(dictList, ofile, null='<NULL>', *args, **kwargs):
-    # """
-    # export data to csv, the first line contains the column names
+    for study in db().select(db.studies.ALL):
+        for t in linkTemps:
+            ref = t %(study.id)
+            links.append(A(ref, _href=ref));
 
-    # :param ofile: where the csv must be exported to
-    # :param null: how null values must be represented (default '<NULL>')
-    # :param delimiter: delimiter to separate values (default ',')
-    # :param quotechar: character to use to quote string values (default '"')
-    # :param quoting: quote system, use csv.QUOTE_*** (default csv.QUOTE_MINIMAL)
-    # :param represent: use the fields .represent value (default False)
-    # :param colnames: list of column names to use (default self.colnames)
-                     # This will only work when exporting rows objects!!!!
-                     # DO NOT use this with db.export_to_csv()
-    # """
-    # delimiter = kwargs.get('delimiter', ',')
-    # quotechar = kwargs.get('quotechar', '"')
-    # quoting = kwargs.get('quoting', csv.QUOTE_MINIMAL)
-    # represent = kwargs.get('represent', False)
-    # writer = csv.writer(ofile, delimiter=delimiter,
-                        # quotechar=quotechar, quoting=quoting)
-                        
-    # # rows are 2 layers deep - one layer for table, one for field
-    # colnames = []
-    # for t, tval in tables.iteritems():
-        # if (isinstance(tval, dict)
-        # for f, fval in tval.iteritems():
-            # colnames.append(t + '.' + f)
-            
-    # colnames = kwargs.get('colnames', dictList[0].keys())
-    # write_colnames = kwargs.get('write_colnames',True)
-    # # a proper csv starting with the column names
-    # if write_colnames:
-        # writer.writerow(colnames)
+    return dict(links=links)
 
-    # def none_exception(value):
-        # """
-        # returns a cleaned up value that can be used for csv export:
-        # - unicode text is encoded as such
-        # - None values are replaced with the given representation (default <NULL>)
-        # """
-        # if value is None:
-            # return null
-        # elif isinstance(value, unicode):
-            # return value.encode('utf8')
-        # elif isinstance(value,Reference):
-            # return long(value)
-        # elif hasattr(value, 'isoformat'):
-            # return value.isoformat()[:19].replace('T', ' ')
-        # elif isinstance(value, (list,tuple)): # for type='list:..'
-            # return bar_encode(value)
-        # return value
+def entry_surveys():
+    study_id = request.args[0]
+    study = db.studies(study_id)
+    
+    stream=cStringIO.StringIO()
+    
+    export_entry_surveys(stream, study)
+    
+    return dump_csv(stream, 'entry_surveys_%s.csv' %(study_id))
+    
+    
+def post_surveys():
+    study_id = request.args[0]
+    study = db.studies(study_id)
+    
+    stream=cStringIO.StringIO()
+    
+    export_post_surveys(stream, study)
+    
+    return dump_csv(stream, 'post_surveys_%s.csv' %(study_id))
 
-    # for record in dictList:
-        # row = []
-        # for col in colnames:
-            # (t, f) = col.split('.')
-            # if t in record and f in record[t]:
-                # value = record[t][f]
-            # else:
-                # value = record[f]
-            
-            # field = db[t][f]
-            # if field.type=='blob' and not value is None:
-                # value = base64.b64encode(value)
-            # elif represent and field.represent:
-                # value = field.represent(value)
-            # row.append(none_exception(value))
-        # writer.writerow(row)
+
+def dump_csv(stream, name='output.csv'):
+    response.headers['Content-Type']='application/vnd.ms-excel'
+    response.headers['Content-Disposition']='attachment;filename=%s' %(name)
+    return stream.getvalue()
--- web2py/applications/utility/models/keystroke.py
+++ web2py/applications/utility/models/keystroke.py
@@ -1,3 +1,5 @@
+import csv
+import gluon.contrib.simplejson as json
 
 options.keystroke = {
     'price' : [0.03, 0.05],
@@ -53,7 +55,7 @@ db.define_table('ks_post_surveys',
 				db.Field('m_fedup', 'double'),
 				db.Field('m_jittery', 'double'),
 				db.Field('m_active', 'double'),
-				# db.Field('m_overall', 'double'),
+				db.Field('m_overall', 'double'),
 				db.Field('free_write_text', 'text'),
                 db.Field('free_write_time', 'double'),
 				db.Field('associations', 'text'),
@@ -118,7 +120,7 @@ def record_post_survey():
     m_jittery = hit_session.mood_form.m_jittery
     m_active = hit_session.mood_form.m_active
     
-    # m_overall = hit_session.mood_form.m_overall
+    m_overall = hit_session.mood_form.m_overall
     
     free_write_text = hit_session.free_write_text
     free_write_time = hit_session.free_write_time
@@ -150,9 +152,95 @@ def record_post_survey():
                     m_fedup=m_fedup,
                     m_jittery=m_jittery,
                     m_active=m_active,
-                    # m_overall=m_overall,
+                    m_overall=m_overall,
                     free_write_text=free_write_text,
                     free_write_time=free_write_time,
                     associations=associations,
                     association_times=association_times
                       )
+
+
+def experimental_vars(study):
+    conditions = json.loads(study.conditions)
+    vars = conditions.keys()
+    return [x for x in vars
+            if isinstance(conditions[x], (list, tuple))]
+
+# this does not help generate the UI
+biometrics = [
+    dict(name='Fingerprints', value='fingerprint'),
+    dict(name='Iris or retina scans', value='eye'),
+    dict(name='Face recognition', value='face'),
+    dict(name='Voice recognition', value='voice'),
+    dict(name='Signature recognition', value='signature'),
+    dict(name='Keystroke (typing) pattern', value='keystroke'),
+    dict(name='Other biometric systems', value='other')
+]
+            
+def export_entry_surveys(stream, study):
+
+    writer = csv.writer(stream)
+
+    conditions = json.loads(study.conditions)
+    variables = experimental_vars(study)
+    biometricValues = [x['value'] for x in biometrics]
+    
+    header = ['id', 'run_length', 'start_time', 'end_time', 'workerid', 'censored', 'other']
+    header += variables
+    header += ['age', 'gender', 'occupation', 'duration']
+    header += biometricValues
+    
+    writer.writerow(header)
+    
+    studyMatches = (db.runs.study==study) & (db.ks_entry_surveys.study == study)
+    entries = db(studyMatches).select(
+        db.runs.ALL,
+        db.ks_entry_surveys.ALL,
+        join=db.runs.on(db.runs.workerid == db.ks_entry_surveys.workerid)
+    )
+    
+    for entry in entries:
+        run = entry.runs
+        es = entry.ks_entry_surveys
+        
+        condition = json.loads(run.condition.json)
+        vals = ([run.id, run.length, run.start_time, run.end_time, run.workerid, run.censored, run.other] 
+                + [condition[key] for key in variables]
+                + [es.age, es.gender, es.occupation, es.duration]
+                + [v in es.biometric for v in biometricValues])
+        writer.writerow(vals)
+    
+def export_post_surveys(stream, study):
+    writer = csv.writer(stream)
+
+    conditions = json.loads(study.conditions)
+    variables = experimental_vars(study)
+    
+    header = ['id', 'run_length', 'start_time', 'end_time', 'workerid', 'censored', 'other']
+    header += variables
+    header += ['time', 'survey_number', 'attempts', 'duration', 
+        'm_lively', 'm_drowsy', 'm_happy', 'm_sad', 'm_overall',
+        'free_write_text', 'free_write_time', 'associations', 'association_times']
+    
+    writer.writerow(header)
+    
+    studyMatches = (db.runs.study==study) & (db.ks_post_surveys.study == study)
+    entries = db(studyMatches).select(
+        db.runs.ALL,
+        db.ks_post_surveys.ALL,
+        join=db.runs.on(db.runs.workerid == db.ks_post_surveys.workerid),
+        orderby=db.runs.start_time|db.ks_post_surveys.time
+    )
+    
+    for entry in entries:
+        run = entry.runs
+        ps = entry.ks_post_surveys
+        
+        condition = json.loads(run.condition.json)
+        vals = ([run.id, run.length, run.start_time, run.end_time, run.workerid, run.censored, run.other] 
+                + [condition[key] for key in variables]
+                + [ps.time, ps.completed, ps.attempts, ps.duration, 
+                    ps.m_lively, ps.m_drowsy, ps.m_happy, ps.m_sad, ps.m_overall,
+                    ps.free_write_text, ps.free_write_time, ps.associations, ps.association_times])
+        writer.writerow(vals)
+    
