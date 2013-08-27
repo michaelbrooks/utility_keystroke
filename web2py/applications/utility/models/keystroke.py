e7539c1cb00111e8802492fd47bb2dc7069742f1
Michael Brooks <mjbrooks@uw.edu>
2013-08-27 23:24:05 +0000

    use post survey in pre survey etc

--- web2py/applications/utility/models/keystroke.py
+++ web2py/applications/utility/models/keystroke.py
@@ -199,25 +199,69 @@ def export_entry_surveys(stream, study):
     header += variables
     header += ['age', 'gender', 'occupation', 'duration']
     header += biometricValues
-    
+    header += ['post_survey_count', 'attempts_sum', 'experienced_rejection_rate', 'duration_mean', 
+        'm_frustrated_mean', 'm_satisfied_mean', 'm_happy_mean', 
+        'm_sad_mean', 'm_overall_mean', 'mood_time_mean',
+        'free_write_time_mean']
+        
     writer.writerow(header)
     
-    studyMatches = (db.runs.study==study) & (db.ks_entry_surveys.study == study)
-    entries = db(studyMatches).select(
+    studyMatches = (db.runs.study==study) & \
+                   (db.ks_entry_surveys.study == study)
+                   
+    join = (db.runs.workerid == db.ks_entry_surveys.workerid)
+    
+    entries = db(studyMatches & join).select(
         db.runs.ALL,
-        db.ks_entry_surveys.ALL,
-        join=db.runs.on(db.runs.workerid == db.ks_entry_surveys.workerid)
+        db.ks_entry_surveys.ALL
     )
     
     for entry in entries:
         run = entry.runs
         es = entry.ks_entry_surveys
         
+        conditions = (db.ks_post_surveys.study == study) & (db.ks_post_surveys.workerid == run.workerid)
+        ps = db(conditions).select(
+            db.ks_post_surveys.id.count().with_alias('post_survey_count'), 
+                db.ks_post_surveys.attempts.sum().with_alias('attempts_sum'),
+                db.ks_post_surveys.duration.avg().with_alias('duration_mean'),
+            db.ks_post_surveys.m_frustrated.avg().with_alias('m_frustrated_mean'),
+                db.ks_post_surveys.m_satisfied.avg().with_alias('m_satisfied_mean'),
+                db.ks_post_surveys.m_happy.avg().with_alias('m_happy_mean'),
+            db.ks_post_surveys.m_sad.avg().with_alias('m_sad_mean'),
+                db.ks_post_surveys.m_overall.avg().with_alias('m_overall_mean'), 
+                db.ks_post_surveys.mood_time.avg().with_alias('mood_time_mean'),
+            db.ks_post_surveys.free_write_time.avg().with_alias('free_write_time_mean'),
+            groupby=db.ks_post_surveys.workerid
+        ).first()
+        
+        if ps:
+            experienced_rejection_rate = float(ps.attempts_sum - ps.post_survey_count) / ps.attempts_sum
+        else:
+            # defaults for simplicity
+            ps = Storage({
+                'post_survey_count': 0,
+                'attempts_sum': 0,
+                'duration_mean': 'NA',
+                'm_frustrated_mean': 'NA',
+                'm_satisfied_mean': 'NA',
+                'm_happy_mean': 'NA',
+                'm_sad_mean': 'NA',
+                'm_overall_mean': 'NA',
+                'mood_time_mean': 'NA',
+                'free_write_time_mean': 'NA'
+            })
+            experienced_rejection_rate = 'NA'
+        
         condition = json.loads(run.condition.json)
         vals = ([run.id, run.length, run.start_time, run.end_time, run.workerid, run.censored, run.other] 
                 + [condition[key] for key in variables]
                 + [es.age, es.gender, es.occupation, es.duration]
-                + [v in es.biometric for v in biometricValues])
+                + [v in es.biometric for v in biometricValues]
+                + [ps.post_survey_count, ps.attempts_sum, experienced_rejection_rate, ps.duration_mean, 
+                    ps.m_frustrated_mean, ps.m_satisfied_mean, ps.m_happy_mean,
+                    ps.m_sad_mean, ps.m_overall_mean, ps.mood_time_mean,
+                    ps.free_write_time_mean])
         writer.writerow(vals)
     
 def export_post_surveys(stream, study):
@@ -225,36 +269,64 @@ def export_post_surveys(stream, study):
 
     conditions = json.loads(study.conditions)
     variables = experimental_vars(study)
+    biometricValues = [x['value'] for x in biometrics]
     
     header = ['id', 'run_length', 'start_time', 'end_time', 'workerid', 'censored', 'other']
     header += variables
+    header += ['age', 'gender', 'occupation', 'duration']
+    header += biometricValues
     header += ['time', 'survey_number', 'attempts', 'duration', 
         'm_frustrated', 'm_satisfied', 'm_happy', 'm_sad', 'm_overall', 'mood_time',
         'free_write_text', 'free_write_time', 'associations', 'association_times']
     
     writer.writerow(header)
     
-    studyMatches = (db.runs.study==study) & (db.ks_post_surveys.study == study)
-    entries = db(studyMatches).select(
+    studyMatches = (db.runs.study==study) & \
+                   (db.ks_post_surveys.study == study) & \
+                   (db.ks_entry_surveys.study == study)
+                   
+    join = (db.runs.workerid == db.ks_post_surveys.workerid) & \
+           (db.runs.workerid == db.ks_entry_surveys.workerid)
+           
+    entries = db(studyMatches & join).select(
         db.runs.ALL,
         db.ks_post_surveys.ALL,
-        join=db.runs.on(db.runs.workerid == db.ks_post_surveys.workerid),
+        db.ks_entry_surveys.ALL,
         orderby=db.runs.start_time|db.ks_post_surveys.time
     )
     
     for entry in entries:
         run = entry.runs
+        es = entry.ks_entry_surveys
         ps = entry.ks_post_surveys
         
         condition = json.loads(run.condition.json)
         vals = ([run.id, run.length, run.start_time, run.end_time, run.workerid, run.censored, run.other] 
                 + [condition[key] for key in variables]
+                + [es.age, es.gender, es.occupation, es.duration]
+                + [v in es.biometric for v in biometricValues]
                 + [ps.time, ps.completed, ps.attempts, ps.duration, 
                     ps.m_frustrated, ps.m_satisfied, ps.m_happy, ps.m_sad, ps.m_overall, ps.mood_time,
                     ps.free_write_text, ps.free_write_time, ps.associations, ps.association_times])
         writer.writerow(vals)
     
 
+def create_indices_on_postgres_ks():
+    '''Creates a set of indices if they do not exist'''
+    ## Edit this list of table columns to index
+    ## The format is [('table', 'column')...]
+    indices = [('runs', 'workerid'),
+               ('ks_entry_surveys', 'workerid'),
+               ('ks_post_surveys', 'workerid')]
+               
+    for table, column in indices:
+        index_exists = db.executesql("select count(*) from pg_class where relname='%s_%s_idx';"
+                                     % (table, column))[0][0] == 1
+        if not index_exists:
+            db.executesql('create index %s_%s_idx on %s (%s);'
+                          % (table, column, table, column))
+        db.commit()
+    
 def process_keystroke_runs(study):
     db((db.runs.study==study)
        & (db.runs.length==0)).delete()
